f19bcaadff63eb34a1dd31e57023cf5eda95dd0a
unknown <negretemarvilean@gmail.com>
2024-06-24 00:16:23 +0800

    text result added

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 const markerSounds = {};
+const overlay = document.getElementById("overlay");
+const overlay_p = document.querySelector("#overlay p");
+
 const audio = document.getElementById("myAudio")
 const btn_play = document.getElementById("btn_play")
 btn_play.addEventListener("click", function () {
@@ -9,9 +12,26 @@ markers.forEach(element => {
     element.addEventListener("markerFound", () => clickFunction(element))
 });
 
+markers.forEach(element => {
+    element.addEventListener("markerLost", () => {
+        overlay.style.display = "none";
+    })
+});
+
+
 const clickFunction = (element) => {
     const asset = element.querySelector("a-entity").getAttribute("obj-model").obj.replace("obj/", '').replace(".obj", "");
     audio.src = "sounds/" + asset + ".mp3"
     console.log("Found: ", asset)
     btn_play.click()
+    overlay.style.display = "block";
+    try {
+        const pattern = Number(asset)
+        console.log(pattern)
+        if (typeof pattern < 10)
+            overlay_p.innerText = "Number: " + asset;
+        else
+            overlay_p.innerText = "Letter: " + asset[0] + " for " + asset;
+    } catch {
+    }
 }
